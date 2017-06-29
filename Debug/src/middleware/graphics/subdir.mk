69ed43a1d63ddb3fd69d505a4e26ba0f2e566433
David Paul <davidpaul@hotmail.de>
2017-06-29 13:31:22 +0200

    -

---
+++ Debug/src/middleware/graphics/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/middleware/graphics/console.c \
+../src/middleware/graphics/stausbar.c 
+
+OBJS += \
+./src/middleware/graphics/console.o \
+./src/middleware/graphics/stausbar.o 
+
+C_DEPS += \
+./src/middleware/graphics/console.d \
+./src/middleware/graphics/stausbar.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/middleware/graphics/%.o: ../src/middleware/graphics/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F469xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
